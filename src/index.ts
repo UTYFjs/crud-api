aa4214dcd88fc432330f5f43e6afa664925a084e
UTYFjs <utyf-90@mail.ru>
2023-06-29 12:40:10 +0300

    feat: add start:prod mode to config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
+import {createServer} from "http";
 import {app} from "./app/app";
 import {cpus} from "os";
+import {config} from "dotenv";
+
+config();
+
+const PORT=process.env.PORT;
+const server = createServer((req, res) => {
+	console.log("start server");
+	res.write("Hello World");
+	res.end();
+});
+server.listen({ host: "localhost", port: PORT, exclusive: true});
+
+
+
 const sayHi = () => {console.log("Hi"); 
 	//console.log(cpus());
+	
+	
 	app();
 };
 sayHi();
