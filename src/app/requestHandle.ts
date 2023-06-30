c2ac26bfb7cf3942a93db854bae1e77dc8f654c6
UTYFjs <utyf-90@mail.ru>
2023-06-30 17:14:04 +0300

    feat: add README.md .env.example, install supertest, jest

--- src/app/requestHandle.ts
+++ src/app/requestHandle.ts
@@ -8,7 +8,6 @@ import { StatusCode, ErrorMessage } from "./constants";
 export const requestHandle = async (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
 	try{
 		if (!req.url?.startsWith("/api/users")) {
-			console.log(req.url);
 			sendError(res, StatusCode.NOT_FOUND, ErrorMessage.REQUEST_URL_FORMAT_INVALID);
 			return;
 		}
@@ -17,11 +16,9 @@ export const requestHandle = async (req: IncomingMessage, res: ServerResponse<In
 		const id = parsedUrl[2] || "";
 		let body: UserInterface;
 		if (parsedUrl.length === 2) {
-			console.log("parsed.lengrh = 2", req.url);
 			switch (req.method) {
 			case "GET":
 				sendResponse(res, StatusCode.OK, usersDb);
-				console.log("GET api/users");
 				break;
 			case "POST":
 				body = await getBodyRequest(req);  
@@ -30,15 +27,12 @@ export const requestHandle = async (req: IncomingMessage, res: ServerResponse<In
 					const user = new User(body.username, body.age, body.hobbies);
 					usersDb.push(user);
 					sendResponse(res, StatusCode.CREATED, user);
-					console.log("userDB current", usersDb);
 				} else {
 					sendError(res, StatusCode.BAD_REQUEST, ErrorMessage.REQUEST_BODY_FORMAT_INVALID);
 				}
-				console.log("POST api/users", body);
 				break;
 			default:
 				sendError(res, StatusCode.METHOD_NOT_ALLOWED, ErrorMessage.METHOD_NOT_ALLOWED);
-				console.log("WRONG METHOD for users should be error");
 			}
 		} else if (id && parsedUrl.length === 3) {
 			let user: UserInterface | undefined;
@@ -51,10 +45,8 @@ export const requestHandle = async (req: IncomingMessage, res: ServerResponse<In
 				return;
 			}
 
-			console.log("parsed.lengrh = 3", req.url, "id", id);
 			switch (req.method) {
 			case "GET":
-				console.log("GET api/users/{userId}");
 				if (user) {
 					sendResponse(res, StatusCode.OK, user);
 				} else {
@@ -73,35 +65,26 @@ export const requestHandle = async (req: IncomingMessage, res: ServerResponse<In
 					}else{
 						sendError(res, StatusCode.NOT_FOUND, ErrorMessage.USER_NOT_FOUND);
 					}
-					console.log("updated database", usersDb);
 				} else {
 					sendError(res, StatusCode.BAD_REQUEST, ErrorMessage.REQUEST_BODY_FORMAT_INVALID);
 				}
-				console.log("PUT api/users/{userId}");
 				break;
 			case "DELETE":
 				
 				// eslint-disable-next-line no-case-declarations
 				const index = usersDb.findIndex((user) => user.id == id);
 				if( index !== -1) {
-					console.log("не найдено");
 					usersDb.splice(index, 1);
 					sendResponse(res, StatusCode.NO_CONTENT, {} as UserInterface);
 				}else{
-					console.log("найдено");
 					sendError(res, StatusCode.NOT_FOUND, ErrorMessage.USER_NOT_FOUND);
 				}
-
-
-				console.log("DELETE api/users/{userId}");
 				break;
 			default:
 				sendError(res, StatusCode.METHOD_NOT_ALLOWED, ErrorMessage.METHOD_NOT_ALLOWED);
-				console.log("WRONG METHOD for userId should be error");
 			}
 		} else {
 			sendError(res, StatusCode.NOT_FOUND, ErrorMessage.NOT_FOUND);
-			console.log("parsed.lengrh >3  Should be error", req.url);
 		}
 	} catch{
 		sendError(res, StatusCode.SERVER_ERROR, ErrorMessage.SERVER_ERROR);
